b9dee4c3d9b9ac91c15e476dbe88619ef2262aa4
CASSIO <cassio.bjhones@gmail.com>
2023-02-23 15:08:20 -0300

    Operadors_logicos

---
+++ tipos-variaveis/src/OpeLogicos.java
@@ -0,0 +1,15 @@
+public class OpeLogicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+    // ----CONDICAO E----CONDICAO E----CONDICAO E----CONDICAO E----
+        if (condicao1 && condicao2) {
+            System.out.println("--AS DUAS SAO VERDADEIRAS--");
+        }
+// ----CONDICAO OU---CONDICAO OU---CONDICAO OU---CONDICAO OU---
+        if (condicao1 || condicao2) {
+            System.out.println("--UMA DELAS É VERDADEIRAS--");
+        }
+
+    }
+}
